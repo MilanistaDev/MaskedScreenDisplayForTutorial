6bec868238201bafe572ee8d4db9101efaca8a30
Takuya Aso <milanista224@gmail.com>
2023-05-22 14:08:50 +0900

    Initial Commit

---
+++ MaskedScreenDisplayForTutorial/MaskedScreenDisplayForTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaskedScreenDisplayForTutorialApp.swift
+//  MaskedScreenDisplayForTutorial
+//
+//  Created by Takuya Aso on 2023/05/22.
+//
+
+import SwiftUI
+
+@main
+struct MaskedScreenDisplayForTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
