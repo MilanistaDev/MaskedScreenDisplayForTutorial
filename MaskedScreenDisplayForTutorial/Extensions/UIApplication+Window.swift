ce192710cac54c3d9002396f0764947586afe5ca
Takuya Aso <milanista224@gmail.com>
2023-06-01 23:39:30 +0900

    Changed to be able to get the height of safearea.

--- MaskedScreenDisplayForTutorial/Extensions/UIApplication+Window.swift
+++ MaskedScreenDisplayForTutorial/Extensions/UIApplication+Window.swift
@@ -8,20 +8,24 @@
 import UIKit
 
 extension UIApplication {
-    /// KeyWindow の rootViewControllerを取得
-    private var rootViewController: UIViewController? {
+    /// KeyWindow 取得
+    private var keyWindow: UIWindow? {
         return UIApplication.shared.connectedScenes
             .compactMap { $0 as? UIWindowScene }
             .first?
             .windows
             .filter { $0.isKeyWindow }
-            .first?
-            .rootViewController
-        }
+            .first
+    }
+
+    /// KeyWindow の rootViewControllerを取得
+    private var rootViewController: UIViewController? {
+        return keyWindow?.rootViewController
+    }
 
     // 最前面のViewControllerを取得
     func frontMostViewController() -> UIViewController? {
-        guard let rootViewController = UIApplication.shared.rootViewController else {
+        guard let rootViewController = rootViewController else {
             return nil
         }
 
@@ -38,7 +42,7 @@ extension UIApplication {
     /// 画面の横幅を取得
     /// - Returns: 画面の横幅
     func screenWidth() -> CGFloat {
-        guard let rootViewController = UIApplication.shared.rootViewController else {
+        guard let rootViewController = rootViewController else {
             return .zero
         }
         return rootViewController.view.frame.width
@@ -47,9 +51,28 @@ extension UIApplication {
     /// 画面の縦幅を取得
     /// - Returns: 画面の縦幅
     func screenHeight() -> CGFloat {
-        guard let rootViewController = UIApplication.shared.rootViewController else {
+        guard let rootViewController = rootViewController else {
             return .zero
         }
         return rootViewController.view.frame.height
+    }
+
+    /// 上部のSafeAreaの高さを取得
+    /// - Returns: 上部のSafeAreaの高さ
+    func safeAreaTopHeight() -> CGFloat {
+        guard let keyWindow = keyWindow else {
+            return .zero
+        }
+        return keyWindow.safeAreaInsets.top
+    }
+
+
+    /// 下部のSafeAreaの高さを取得
+    /// - Returns: 下部のSafeAreaの高さ
+    func safeAreaBottomHeight() -> CGFloat {
+        guard let keyWindow = keyWindow else {
+            return .zero
+        }
+        return keyWindow.safeAreaInsets.bottom
     }
 }
