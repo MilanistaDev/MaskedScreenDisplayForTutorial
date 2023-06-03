3b1093b9bd7fe1df4bfb81ff30dce68a4333f0e3
Takuya Aso <milanista224@gmail.com>
2023-06-03 23:00:44 +0900

    Position adjustment.

--- MaskedScreenDisplayForTutorial/Views/MaskedCircleView.swift
+++ MaskedScreenDisplayForTutorial/Views/MaskedCircleView.swift
@@ -8,15 +8,35 @@
 import SwiftUI
 
 struct MaskedCircleView: View {
+    private let padding = 20.0
+    private let buttonRadius = 24.0
+    private let tabBarHeight = 49.0
+
     var body: some View {
         Circle()
             .frame(width: 64, height: 64)
             .position(
-                x: UIApplication.shared.screenWidth() - 44,
-                y: UIApplication.shared.screenHeight() - 127)
+                x: xPosition(),
+                y: yPosition())
             .background(Color.white)
             .compositingGroup()
             .luminanceToAlpha()
+    }
+}
+
+extension MaskedCircleView {
+    private func xPosition() -> CGFloat {
+        // 画面幅 - 画面右端とボタンまでのパディング - ボタンの半径
+        UIApplication.shared.screenWidth() - padding - buttonRadius
+    }
+
+    private func yPosition() -> CGFloat {
+        // 画面の高さ - Safe Area下部の高さ - タブバーの高さ - タブバーとボタンまでのパディング - ボタンの半径
+        UIApplication.shared.screenHeight() -
+        UIApplication.shared.safeAreaBottomHeight() -
+        tabBarHeight -
+        padding -
+        buttonRadius
     }
 }
 
